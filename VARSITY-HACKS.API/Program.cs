24da26b61286b212b176f4acb3a1a1c6dba8569b
Shahed Al Mahmud <shahedloopsit@gmail.com>
2022-02-27 09:36:50 +0600

    Automapper

--- VARSITY-HACKS.API/Program.cs
+++ VARSITY-HACKS.API/Program.cs
@@ -33,6 +33,7 @@ builder.Services.AddCors();
 
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen(options =>
