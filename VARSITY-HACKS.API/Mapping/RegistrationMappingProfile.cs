cca9d781dbd1847d0a8c15ac0e4146de7c3dc628
Shahed Al Mahmud <shahedloopsit@gmail.com>
2022-02-28 09:04:36 +0600

    Personality error solved

--- VARSITY-HACKS.API/Mapping/RegistrationMappingProfile.cs
+++ VARSITY-HACKS.API/Mapping/RegistrationMappingProfile.cs
@@ -10,7 +10,7 @@ public class RegistrationMappingProfile: Profile
     {
         CreateMap<Registration, RegistrationEditModel>()
             .ForMember(d => d.Personality, opt => opt.MapFrom(c => c.Personality.ToString()))
-            .ForMember(d => d.Personality, opt => opt.MapFrom(c => c.Personality.GetDescription()))
+            .ForMember(d => d.PersonalityDescription, opt => opt.MapFrom(c => c.Personality.GetDescription()))
             .ReverseMap();
         CreateMap<RegistrationCreateModel, Registration>()
             .ForMember(d=> d.Email, opt=> opt.MapFrom(c=> c.UserName))
