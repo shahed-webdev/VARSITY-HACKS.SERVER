fed23bfaa84907853075e7bc836b87dea793ea9b
Shahed Al Mahmud <shahedloopsit@gmail.com>
2022-03-31 12:51:06 +0600

    ExternalLogin token

--- VARSITY-HACKS.API/Controllers/AuthController.cs
+++ VARSITY-HACKS.API/Controllers/AuthController.cs
@@ -133,13 +133,32 @@ namespace VARSITY_HACKS.API.Controllers
             }
 
             var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, false);
+            
+            var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, email),
+                new Claim(ClaimTypes.Name, email),
+                new Claim(ClaimTypes.Email, email),
+            };
+            var token = new JwtSecurityToken
+            (
+                issuer: _config["Jwt:Issuer"],
+                audience: _config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(60),
+                notBefore: DateTime.UtcNow,
+                signingCredentials: new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"])), SecurityAlgorithms.HmacSha256)
+            );
+
             if (result.Succeeded)
             {
-                return Redirect(returnUrl);
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                return Ok(new ResponseModel<string>(true, "Token", tokenString));
             }
             else
             {
-                var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+               
                 var user = new IdentityUser() { UserName = email, Email = email };
                 var createResult = await _userManager.CreateAsync(user);
                
@@ -156,7 +175,10 @@ namespace VARSITY_HACKS.API.Controllers
                 }
                 
                 await _signInManager.SignInAsync(user, false);
-                return Redirect(returnUrl);
+
+                var tokenString = new JwtSecurityTokenHandler().WriteToken(token);
+                return Ok(new ResponseModel<string>(true, "Token", tokenString));
+               
             }
         }
 
